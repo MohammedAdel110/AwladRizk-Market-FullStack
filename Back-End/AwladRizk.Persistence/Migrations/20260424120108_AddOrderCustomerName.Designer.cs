c23dc30075d0275828f89dd7290a2b4f2330a612
Mohammed Adel <145216355+MohammedAdel110@users.noreply.github.com>
2026-04-24 15:06:37 +0300

    f

--- Back-End/AwladRizk.Persistence/Migrations/20260424120108_AddOrderCustomerName.Designer.cs
+++ Back-End/AwladRizk.Persistence/Migrations/20260424120108_AddOrderCustomerName.Designer.cs
@@ -4,6 +4,7 @@ using AwladRizk.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AwladRizk.Persistence.Migrations
 {
     [DbContext(typeof(AwladRizkDbContext))]
-    partial class AwladRizkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260424120108_AddOrderCustomerName")]
+    partial class AddOrderCustomerName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
